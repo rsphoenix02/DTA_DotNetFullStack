735f9ca167adb90a694a3528df8a2b32506ab91b
rsphoenix02 <74128672+rsphoenix02@users.noreply.github.com>
2024-09-10 16:55:01 +0530

    Add files via upload

---
+++ API_Assignment/Controllers/Products.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Model;
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Products : ControllerBase
+    {
+        ProductsDbContext _context;
+
+        public Products(ProductsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(_context.Products.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            Product obj = _context.Products.Find(id);
+
+            if (obj == null)
+            {
+                return BadRequest("Product with ID: " + id + " does not exist.");
+            }
+            else return Ok(obj);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product obj)
+        {
+            _context.Products.Add(obj);
+            _context.SaveChanges();
+            return Ok("New product added successfully!");
+        }
+
+        [HttpPut]
+        public IActionResult EditProduct(Product obj)
+        {
+            _context.Products.Update(obj);
+            _context.SaveChanges();
+            return Ok("Product details updated successfully!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+               Product obj = _context.Products.Find(id);
+
+            if (obj == null) return BadRequest("Product with ID: " + id + "not found.");
+
+            _context.Products.Remove(obj);
+            _context.SaveChanges();
+            return Ok("Product deleted successfully.");
+        }
+
+    }   
+}
---
+++ API_Assignment/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductsDbContext")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
