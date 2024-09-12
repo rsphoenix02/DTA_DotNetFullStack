d8e6768d17c485aeae6561a795d3ccd5d29b84b0
rsphoenix02 <74128672+rsphoenix02@users.noreply.github.com>
2024-09-12 12:37:03 +0530

    Update Program.cs

--- DotNetCore_Advanced_Project/Program.cs
+++ DotNetCore_Advanced_Project/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebApplication4.Model;
 using WebApplication4.Repositories;
 using WebApplication4.Services;
@@ -19,6 +22,43 @@ builder.Services.AddDbContext<ProductsDbContext>(options => options.UseSqlServer
 
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IProductService, ProductService>();
+
+builder.Services.AddAuthentication(options =>
+
+{
+
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options =>
+
+{
+
+    options.SaveToken = true;
+
+    options.RequireHttpsMetadata = false;
+
+    options.TokenValidationParameters = new TokenValidationParameters()
+
+    {
+
+        ValidateIssuer = true,
+
+        ValidateAudience = true,
+
+        ValidAudience = "myUsers",
+
+        ValidIssuer = "mySystem",
+
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is jwt token required secret key used to generate jwt token required for authentiation"))
+
+    };
+
+});
+
 
 var app = builder.Build();
 
