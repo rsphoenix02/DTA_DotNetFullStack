17d2ba41b65f0cb33ec1c5b74c6e2d76d947d121
rsphoenix02 <74128672+rsphoenix02@users.noreply.github.com>
2024-09-12 12:35:41 +0530

    Create LoginModel.cs

---
+++ DotNetCore_Advanced_Project/Model/LoginModel.cs
@@ -0,0 +1,30 @@
+namespace WebApplication4.Model
+
+{
+
+    public class UserModel
+
+    {
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+    }
+
+    public class LoginModel
+
+    {
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+    }
+
+}
+
